446175e9659b2357290873d31ed89ba391efd0ad
윤효진 <hyojinee@dgsw.hs.kr>
2025-01-13 16:05:49 +0900

    feat :: add useQuery

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 
 function App() {
-    const info = useQuery({ queryKey: ['todos'], queryFn : fetchTodoList })
+    const info = useQuery({
+        queryKey: ['todos'],
+        queryFn : fetchTodoList,
+    });
 }
