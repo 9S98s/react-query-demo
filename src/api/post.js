090de36fff508b11a3a2d38da1637e3f3e46dea0
윤효진 <hyojinee@dgsw.hs.kr>
2025-01-30 10:46:35 +0900

    주석 삭제

--- src/api/post.js
+++ src/api/post.js
@@ -1,6 +1,5 @@
 import axios from 'acxios';
 
-// POST 요청: 새로운 게시글 생성
 export const createPost = async (newPost) => {
     const response = await axios.post('https://api.example.com/posts', newPost);
     return response.data;
